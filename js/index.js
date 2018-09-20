3bd3a064750748e404c7519e8665839e813a3729
roadToDev <o.hlebov@gmail.com>
2018-09-20 22:04:51 +0300

    Buttons added

--- js/index.js
+++ js/index.js
@@ -16,17 +16,49 @@ function parseJsonData (url) {
     })
     .then(function (myJson) {
       showTable(myJson)
-      console.log(myJson)
     })
 }
 
-function showTable (opportunities) {
-  $(document).ready(function(){
+function showTable (jsonData) {
+  $(document).ready(function () {
+    jsonData.oppos.forEach(function (item) {
+      $('#oppos-table').append('<tr><td>17:52</td><td>' + item.name + '</td><td>' + addStages(jsonData, item.name) + '</td></tr>')
+    })
+  })
+}
 
-    $('#1').append('<td id="2">2</td>');
+function addStages (jsonData, oppoName) {
+  var buttons = ''
+  jsonData.stages.forEach(function (stageName) {
+    buttons = buttons + ' ' + addButton(jsonData, oppoName, stageName)
+  })
+  return buttons
+}
 
-  });
-
+function addButton (jsonData, oppoName, stageName) {
+  var button = ''
+  jsonData.oppos.forEach(function (item) {
+    if (item.name === oppoName) {
+      item.stages.forEach(function (item) {
+        if (item.name === stageName) {
+          switch (item.stageStatus.statusStr) {
+            case 'CompletedStStatus':
+              button = '<button type="button" class="btn btn-success btn-sm">' + stageName + '</button>'
+              break
+            case 'ErrorStStatus':
+              button = '<button type="button" class="btn btn-danger btn-sm">' + stageName + '</button>'
+              break
+            case 'InProcessStStatus':
+              button = '<button type="button" class="btn btn-success btn-sm">' + stageName + '</button>'
+              break
+            default:
+              button = '<button type="button" class="btn btn-secondary btn-sm">' + stageName + '</button>'
+          }
+        }
+      })
+    }
+  })
+  return button
 }
 
 parseJsonData('data.json')
